8c968266b01a7de9dae4bde41c865c8c210036d5
Gabriel Kenji Poli Ozaki <g.kenjiJS@hotmail.com>
2023-04-21 13:25:46 -0300

    :lipstick: feat: Estilos Globais do Design System

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
+import { globalStyles } from '@/styles/global'
+
+globalStyles()
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
