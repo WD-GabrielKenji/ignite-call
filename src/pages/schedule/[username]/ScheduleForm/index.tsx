4c32c74f572c105f5f7fd974d4ca43222dca73f9
Gabriel Kenji Poli Ozaki <g.kenjiJS@hotmail.com>
2023-05-01 14:30:19 -0300

    :sparkles: feat: Consumindo os Meses no Calendar via DayJS

--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,6 +1,6 @@
-// import { CalendarStep } from './CalendarStep'
-import { ConfirmStep } from './ConfirmStep/index.page'
+import { CalendarStep } from './CalendarStep'
+// import { ConfirmStep } from './ConfirmStep/index.page'
 
 export function ScheduleForm() {
-  return <ConfirmStep />
+  return <CalendarStep />
 }
