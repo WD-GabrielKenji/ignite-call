5534db8e251e2c32b1666a0468d8e8bc300d866c
Gabriel Kenji Poli Ozaki <g.kenjiJS@hotmail.com>
2023-05-01 13:26:08 -0300

    :sparkles: feat: Confirm da Page Schedule (estático)

--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,5 +1,6 @@
-import { CalendarStep } from './CalendarStep'
+// import { CalendarStep } from './CalendarStep'
+import { ConfirmStep } from './ConfirmStep/index.page'
 
 export function ScheduleForm() {
-  return <CalendarStep />
+  return <ConfirmStep />
 }
